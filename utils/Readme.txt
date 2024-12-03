d90203c4eabac4a36ed7c508bee1eb75a6b0d465
YourName <jonas.sundberg@slu.se>
2024-12-03 17:11:21 +0100

    update plotting

--- utils/Readme.txt
+++ utils/Readme.txt
@@ -10,3 +10,37 @@ Pipeline:
 
 Activate venv by: 
 
+
+# Check results: 
+# Correct match between event and raw data: 
+- DGT2 2023  
+- DGT1 2023
+- DGT2 2024  
+- DGT1 2024
+
+# But Far3_scale 2024 looks weird, events and data doesn't seem to match!
+# Far3_scale name is correct but event identifier does not work perfectly in 2024 data due to calibration error. 
+# Maybe just skip generating videos for this one!
+
+
+# Does name in original db + event_db match the actual video footage?
+# Check one film from each camera each year (total: 16 films) to match between event time and video time
+
+2023-06-27 
+all cameras 11:00... 
+
+2024-06-27
+all cameras 04:00... 
+
+
+
+# Scales 2025: 
+1 FAR3BONDEN3 no change
+2 FAR6BONDEN6 no change
+3 BJORN3TRI3 no change
+4 BJORN6TRI6? new, similiar setup
+5 FAR3? better camera (side)
+6 FAR6? better camera (side)
+7  RAZORBILL LEDGE w camera?
+8  RAZORBILL LEDGE w camera? 
+
--- utils/plot_db_simple.py
+++ utils/plot_db_simple.py
@@ -21,14 +21,13 @@ def load_db2(db_path: Path):           #load database and change into dataframe#
 
 
 dgt = "dgt2"                    
-date = "20230509"
+date = "20240627"
 yr = int(date[0:4])
 filename = f'{date}_{dgt}.db'
-db_path = list(Path(f'../../../../../../mnt/BSP_NAS2/Other_sensors/weightlog/').rglob(filename))[0]
 
-
+#db_path = list(Path(f'../../../../../../mnt/BSP_NAS2/Other_sensors/weightlog/').rglob(filename))[0]
+db_path = list(Path(f'../../../../../../Volumes/JHS-SSD2/dgt/').rglob(filename))[0]
 #db_path = Path(f"C:/Users/Katharina/Documents/scaledata/{date}_{dgt}.db")
-#db_path = Path(f'../../../../../../Volumes/JHS-SSD2/dgt/{yr}/{dgt}/{date}/{date}_{dgt}.db')
 #db_path = Path(f'../../../../../../Volumes/JHS-SSD2/dgt/{yr}/backup/{date}/{date}_{dgt}.db')
 #db_path = Path(f'../../../../../../mnt/BSP_NAS2/Other_sensors/weightlog/{yr}/{dgt}/{date}/{date}_{dgt}.db')
 
@@ -56,47 +55,46 @@ df_events = df_events[cond1 & cond2]
 df["time"] = pd.to_datetime((1000*60*60*2)+df["timestamp"], unit='ms')  #changes timestamp into local time#
 
 
-#for row in lookup.index:
-#    start = lookup["Startdate"][row]
-#    end = lookup["Enddate"][row]+pd.Timedelta(days = 1)
-#    interval.append(pd.Interval(start, end, closed = "neither"))
+# Assign correct scale names to rows
+lookup = pd.read_csv("temp/ScaleSystemNames.csv", sep = ";", parse_dates=["Startdate", "Enddate"])
+interval = []
+for row in lookup.index:
+    start = lookup["Startdate"][row]
+    end = lookup["Enddate"][row]+pd.Timedelta(days = 1)
+    interval.append(pd.Interval(start, end, closed = "neither"))
 
-#lookup["Interval"] = interval
+lookup["Interval"] = interval
 
 # Pick out matching dates (only first row of data) 
-#time = df["time"][0]
-#inside = []
-#for row in lookup.index: 
-#    if time in lookup["Interval"][row]:
-#        inside.append(1)
-#    else: 
-#        inside.append(0) 
+time = df["time"][0]
+inside = []
+for row in lookup.index: 
+    if time in lookup["Interval"][row]:
+        inside.append(1)
+    else: 
+        inside.append(0) 
 
-#lookup["Inside"] = inside
-#cond1 = lookup["Inside"] == 1
-#cond2 = lookup["DGT"] == dgt
-#lookup_reduced = lookup[cond1 & cond2]
-#lookup_reduced = lookup_reduced.sort_values(["Cell"])
-#names = lookup_reduced["Cameraname"]
+lookup["Inside"] = inside
+cond1 = lookup["Inside"] == 1
+cond2 = lookup["DGT"] == dgt
+lookup_reduced = lookup[cond1 & cond2]
+lookup_reduced = lookup_reduced.sort_values(["Cell"])
+names = lookup_reduced["Cameraname"]
 
 # Merge based on date, dgt name and cell num
-#df.rename(columns={'cell_1': names.iloc[0],'cell_2':names.iloc[1],'cell_3': names.iloc[2],'cell_4':names.iloc[3]},inplace=True)
-
-
-
-names = ["FAR3BONDEN3", "BJORN3TRI3", "FAR6BONDEN6", "FAR6"]
+df.rename(columns={'cell_1': names.iloc[0],'cell_2':names.iloc[1],'cell_3': names.iloc[2],'cell_4':names.iloc[3]},inplace=True)
 
 
 ## Simple plot of time series of raw data for the four cells
 fig, ax = plt.subplots(4)
 for i in range(1,5):
     ax[(i-1)].plot(df["time"], df.iloc[:,(i)])
-    name = names[i-1]
+    name = df.columns[i]
     ax[(i-1)].set_title(name)
-    evdat = df_events
+    evdat = df_events[df_events["Cameraname"] == name]
     for j in evdat.index:
-        ax[(i-1)].vlines(x = evdat.loc[j]["Event_start_time_corr"], ymin = 0, ymax = 1, colors = "green")
-        ax[(i-1)].vlines(x = evdat.loc[j]["Event_end_time_corr"], ymin = 0, ymax = 1, colors = "red")
+        ax[(i-1)].vlines(x = evdat.loc[j]["Event_start_time"], ymin = 0, ymax = 1, colors = "green")
+        ax[(i-1)].vlines(x = evdat.loc[j]["Event_end_time"], ymin = 0, ymax = 1, colors = "red")
 
 plt.suptitle(f'{dgt}_{date}')
 
