582cfece741b07919c87a342e569c1741d550326
hentatisundberg <jonas.sundberg@slu.se>
2026-03-26 10:18:14 +0100

    Checkpoint from VS Code for cloud agent session

--- utils/create_event_vids.py
+++ utils/create_event_vids.py
@@ -9,11 +9,28 @@ import os
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import imageio_ffmpeg
 
-events = df_from_db("out/Events23-24_weights1.db", "event", "Event_start>0", "Sec_start>0.1", False)
+events = df_from_db("out/Events23-25_weights.db", "event", "Event_start>0", "Sec_start>0.1", False)
 
 # Build path to raw video
-vid_path = Path("../../../../../../mnt/BSP_NAS2/Video/")
+
+# General paths for the different years: 
+vid_path_2023 = Path("../../../../../../mnt/BSP_NAS2/Video/Video2023/")
+vid_path_2024 = Path("../../../../../../mnt/BSP_NAS2/Video/Video2024/")
+vid_path_2025 = Path("../../../../../../mnt/BSP_NAS2_vol4/Video/Video2025/")
+
+# Dictionary for video names in 2025 
+name_aliases_2025 = {
+    "BJORN3TRI3_SCALE": "BJORN3TRI3_SCALE_ (192.168.1.110)",
+    "FAR3_SCALE": "FAR3_SCALE_(192.168.1.161)",
+    "BJORN3TRI3_SCALE": "BJORN3TRI3_SCALE_ (192.168.1.110)",
+    "FAR6BONDEN6_SCALE": "FAR6BONDEN6_SCALE_(192.168.1.147)",
+    "TRI2_SCALE": "TRI2_SCALE _(192.168.1.161)",
+    "TRI5_SCALE": "TRI5_SCALE_(192.168.1.161)"    
+}
+
+
 output_path = "../../../../../../mnt/BSP_NAS2_work/temp/eventvids_imageio2/"
+os.makedirs(output_path, exist_ok=True)
 
 for rows in events.index: 
 
@@ -40,19 +57,20 @@ for rows in events.index:
 
         if os.path.isfile(vidfile):
 
-            T1, T2 = [int(t) for t in [startsec, endsec]]
+            T1 = max(0, int(startsec))
+            T2 = max(T1 + 1, int(endsec))
             
             try: 
                 ffmpeg_extract_subclip(
                     vidfile,   
                     T1,
-                    T1+T2,
+                    T2,
                     filename_out)
                 
                 print(f'{filename_out} OK!')
 
-            except:
-                print(f'{filename_out} fail')
+            except Exception as exc:
+                print(f'{filename_out} fail: {exc}')
                 continue
 
         else:    
